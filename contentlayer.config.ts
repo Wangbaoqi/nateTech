21e7bb10626121b11ee155afcab10f82f23d1f58
nate.wang <wangbaoqi8839@gmail.com>
2024-03-14 22:06:24 +0800

    fix: update build fail reason that fetch local routes

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,10 +1,10 @@
 // contentlayer.config.ts
-import { defineDocumentType } from 'contentlayer/source-files';
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
 import remarkGfm from 'remark-gfm';
 import rehypeSlug from 'rehype-slug';
 import { visit } from 'unist-util-visit';
 import { spawn } from 'node:child_process';
-import { makeSource } from 'contentlayer/source-remote-files';
+// import { makeSource } from 'contentlayer/source-remote-files';
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -46,65 +46,6 @@ export const Docs = defineDocumentType(() => ({
   }
 }));
 
-const runBashCommand = (command: string) =>
-  new Promise((resolve, reject) => {
-    const child = spawn(command, [], { shell: true });
-
-    // child.stdout.setEncoding('utf8');
-
-    child.stdout.on('data', (data) => {
-      console.log(Buffer.from(data, 'base64'), 'update data');
-      return process.stdout.write(data);
-    });
-
-    child.stderr.setEncoding('utf8');
-    child.stderr.on('data', (data) => process.stderr.write(data));
-
-    child.on('close', function (code) {
-      if (code === 0) {
-        console.log(code, 'close code');
-        resolve(void 0);
-      } else {
-        reject(new Error(`Command failed with exit code ${code}`));
-      }
-    });
-  });
-
-const syncContentFromGit = async (contentDir: string) => {
-  const syncRun = async () => {
-    const gitUrl = 'https://github.com/Wangbaoqi/leetCode.git';
-    await runBashCommand(`
-      if [ -d  "${contentDir}" ];
-        then
-          cd "${contentDir}"; git pull;
-        else
-          git clone --depth 1 --single-branch ${gitUrl} ${contentDir};
-      fi
-    `);
-  };
-
-  let wasCancelled = false;
-  let syncInterval: NodeJS.Timeout;
-
-  const syncLoop = async () => {
-    console.log('Syncing content files from git');
-
-    await syncRun();
-
-    if (wasCancelled) return;
-
-    syncInterval = setTimeout(syncLoop, 1000 * 60);
-  };
-
-  // Block until the first sync is done
-  await syncLoop();
-
-  return () => {
-    wasCancelled = true;
-    clearTimeout(syncInterval);
-  };
-};
-
 export const Algo = defineDocumentType(() => ({
   name: 'Algo',
   filePathPattern: `content/algo/**/*.mdx`,
@@ -128,9 +69,7 @@ export const Algo = defineDocumentType(() => ({
 
 export default makeSource({
   contentDirPath: 'content',
-  syncFiles: () => syncContentFromGit('content/leetCode'),
-  documentTypes: [Post, Algo, Docs],
-  contentDirInclude: ['docs', 'blog', 'leetCode/src'],
+  documentTypes: [Post, Docs, Algo],
   mdx: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [
